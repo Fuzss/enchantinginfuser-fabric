cbca5f05671d968977b559dfb1e990f3742ff4c1
Fuzss <28218241+Fuzss@users.noreply.github.com>
2021-12-03 11:10:25 +0100

    fix isHovered being wrongly used

--- src/main/java/fuzs/enchantinginfuser/registry/ModRegistry.java
+++ src/main/java/fuzs/enchantinginfuser/registry/ModRegistry.java
@@ -17,8 +17,8 @@ import net.minecraft.world.level.material.MaterialColor;
 
 public class ModRegistry {
     private static final RegistryManager REGISTRY = RegistryManager.of(EnchantingInfuser.MOD_ID);
-    public static final Block INFUSER_BLOCK = REGISTRY.registerBlockWithItem("enchanting_infuser", () -> new InfuserBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().strength(5.0F, 1200.0F)), CreativeModeTab.TAB_DECORATIONS);
-    public static final Block ADVANCED_INFUSER_BLOCK = REGISTRY.registerBlockWithItem("advanced_enchanting_infuser", () -> new AdvancedInfuserBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().strength(5.0F, 1200.0F)), CreativeModeTab.TAB_DECORATIONS);
+    public static final Block INFUSER_BLOCK = REGISTRY.registerBlockWithItem("enchanting_infuser", () -> new InfuserBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().lightLevel(blockState -> 7).strength(5.0F, 1200.0F)), CreativeModeTab.TAB_DECORATIONS);
+    public static final Block ADVANCED_INFUSER_BLOCK = REGISTRY.registerBlockWithItem("advanced_enchanting_infuser", () -> new AdvancedInfuserBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED).requiresCorrectToolForDrops().lightLevel(blockState -> 7).strength(5.0F, 1200.0F)), CreativeModeTab.TAB_DECORATIONS);
     public static final BlockEntityType<InfuserBlockEntity> INFUSER_BLOCK_ENTITY_TYPE = REGISTRY.registerRawBlockEntityType("enchanting_infuser", () -> FabricBlockEntityTypeBuilder.create(InfuserBlockEntity::new, INFUSER_BLOCK, ADVANCED_INFUSER_BLOCK));
     public static final MenuType<InfuserMenu> INFUSING_MENU_TYPE = REGISTRY.registerRawMenuType("infusing", () -> InfuserMenu::new);
 
